db3997263828f063263ab49c997dceda17fe4ff8
Pratyush <96726614+pratyush-1@users.noreply.github.com>
2023-10-23 21:03:39 +0530

    Add files via upload

--- dataloaders/camo_dataset.py
+++ dataloaders/camo_dataset.py
@@ -5,7 +5,7 @@ import cv2
 import skimage.exposure
 import numpy as np
 from numpy.random import default_rng
-
+import torchvision
 import torch.utils.data as data
 import torchvision.transforms as transforms
 import random
@@ -17,7 +17,7 @@ from segment_anything.utils.transforms import ResizeLongestSide
 from torchvision.transforms.functional import resize
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-
+import time
 def iou(mask1, mask2):
     # print("___", np.unique(mask1), np.unique(mask2))
     intersection = (mask1 * mask2).sum()
@@ -32,8 +32,8 @@ def iou(mask1, mask2):
 def cv_random_flip(imgs, label):
     # left right flip
     flip_flag = random.randint(0, 1)
-    imgs = imgs.transpose(Image.FLIP_LEFT_RIGHT)
     if flip_flag == 1:
+        imgs = imgs.transpose(Image.FLIP_LEFT_RIGHT)
         for i in range(len(label)):
             label[i] = label[i].transpose(Image.FLIP_LEFT_RIGHT)
     return imgs, label
@@ -118,6 +118,7 @@ class VideoDataset(data.Dataset):
         self.seq_len = seq_len
         self.trainsize = trainsize
         self.transform = ResizeLongestSide(trainsize)
+        self.resize_gt = ResizeLongestSide(256)
         self.image_list = []
         self.gt_list = []
         self.extra_info = []
@@ -167,6 +168,7 @@ class VideoDataset(data.Dataset):
         
         
         ####### CAAD10K-V3
+        
         path = os.path.join(self.dataset_root, "COD10K-v3") 
         
         split_path = os.path.join(path, "Test")
@@ -189,29 +191,29 @@ class VideoDataset(data.Dataset):
                     self.gt_list.append(os.path.join(gt_path, name+'.png'))
                   
         count = len(self.image_list)
-        print(f'{"Train" if finetune else "Test"} Loaded CAAD10K', len(self.image_list) - count)
+        print(f'{"Train" if finetune else "Test"} Loaded COD10K', len(self.image_list))
         count = len(self.image_list)
         assert len(self.image_list) == len(self.gt_list)
           
           
         ###NC4K
-        path = os.path.join(self.dataset_root, "nc4k")
-        split_path = os.path.join(path, "fix_rank_test_dataset")
+        # path = os.path.join(self.dataset_root, "nc4k")
+        # split_path = os.path.join(path, "fix_rank_test_dataset")
         
-        if finetune:
-            split_path = os.path.join(path, "fix_rank_dataset2000")
+        # if finetune:
+        #     split_path = os.path.join(path, "fix_rank_dataset2000")
         
-        img_path = os.path.join(split_path, "img")
-        gt_path = os.path.join(split_path, "instance")
+        # img_path = os.path.join(split_path, "img")
+        # gt_path = os.path.join(split_path, "instance")
 
 
-        for i in sorted(os.listdir(img_path)):
-            self.image_list.append(os.path.join(img_path, i))
-            self.gt_list.append(os.path.join(gt_path, i.split(".")[0]+".png"))        
+        # for i in sorted(os.listdir(img_path)):
+        #     self.image_list.append(os.path.join(img_path, i))
+        #     self.gt_list.append(os.path.join(gt_path, i.split(".")[0]+".png"))        
 
-        print(f'{"Train" if finetune else "Test"} Loaded NC4K', len(self.image_list) - count)
-        count = len(self.image_list)
-        assert len(self.image_list) == len(self.gt_list)
+        # print(f'{"Train" if finetune else "Test"} Loaded NC4K', len(self.image_list) - count)
+        # count = len(self.image_list)
+        # assert len(self.image_list) == len(self.gt_list)
         
         
         ####CAMO
@@ -237,10 +239,12 @@ class VideoDataset(data.Dataset):
         """Normalize pixel values and pad to a square input."""
         # Pad
         h, w = x.shape[-2:]
-        padh = self.trainsize - h
-        padw = self.trainsize - w
+        padh = 256 - h
+        padw = 256 - w
         x = F.pad(x, (0, padw, 0, padh))
+        # x = torchvision.transforms.functional.resize(x.unsqueeze(0), (256, 256), interpolation=torchvision.transforms.InterpolationMode.BILINEAR, antialias=True) 
         return x
+    
     
     def sample_point_uniform(self):
         if len(self.uniform_point_prompt) == 0:
@@ -296,13 +300,18 @@ class VideoDataset(data.Dataset):
         all_gt_copy = all_gt.copy()
         
         for i in range(len(all_gt_copy)):
-            all_gt_copy[i] = self.gt_transform(all_gt_copy[i])[0]
-        
-            all_gt[i] = Image.fromarray(self.preprocess(torch.as_tensor(self.transform.apply_image(np.asarray(all_gt[i], dtype=np.uint8)))
-            ).numpy().astype(np.uint8)*255).resize((256,256))
-            
-            all_gt[i] = self.gt_transform(all_gt[i])[0]        
+            # print(all_gt_copy)
+            all_gt_copy[i] = self.gt_transform(all_gt_copy[i])[0]*255
+            # print(np.unique(all_gt_copy[i]))
+            # print(f'all_gt__shape {self.preprocess(torch.as_tensor(self.transform.apply_image(np.asarray(all_gt[i], dtype=np.uint8)))).shape}') #1,256,256
+            # all_gt[i] = Image.fromarray(self.preprocess(torch.as_tensor(self.transform.apply_image(np.asarray(all_gt[i], dtype=np.uint8)))).numpy().astype(np.uint8)*255).resize((256,256))
+            # print(all_gt[i].shape)
+            # print(f'all_gt_{np.unique(all_gt_[i])}')
+            # all_gt[i] = self.gt_transform(all_gt[i])[0]        
 
+            all_gt[i] = self.preprocess(torch.as_tensor(self.resize_gt.apply_image(np.asarray(all_gt[i],dtype=np.uint8))))
+            # print(all_gt[i].shape)
+            # print(np.unique(all_gt[i]))
         all_point_prompt = None
         all_label_prompt = None
         all_mask_prompt = None
@@ -322,7 +331,7 @@ class VideoDataset(data.Dataset):
         if is_mask_prompt:
             all_mask_prompt = []
         
-        for gt_mask in all_gt:     #### for each object/instance in the image   
+        for gt_mask, gt_mask_ in zip(all_gt, all_gt_copy):     #### for each object/instance in the image   
 
             ### SAMPLING PROMPTS
 
@@ -383,7 +392,7 @@ class VideoDataset(data.Dataset):
                             break
 
             if is_point_prompt: #point prompt
-                possible_points = list(gt_mask.numpy().nonzero())
+                possible_points = list(gt_mask_.numpy().nonzero())
                 if len(possible_points[0]) != 0:
                     point_prompt = []
                     if not self.is_validation_prompt_mid:
@@ -393,7 +402,7 @@ class VideoDataset(data.Dataset):
                         point_prompt = np.asarray(point_prompt)  
                         label_prompt = np.asarray([1]*n_prompt_points)
                     else:
-                        mid = self.get_midpoint(gt_mask.numpy())
+                        mid = self.get_midpoint(gt_mask_.numpy())
                         # print(mid)
                         possible_points = [list(x) for x in zip(possible_points[0], possible_points[1])]
                         # print(possible_points)
@@ -403,7 +412,7 @@ class VideoDataset(data.Dataset):
                         label_prompt = np.asarray([1])
                         # print(point_prompt)
 
-                    all_point_prompt.append(torch.as_tensor(self.transform.apply_coords(point_prompt,  all_gt_copy[0].shape), dtype=torch.float))
+                    all_point_prompt.append(torch.as_tensor(point_prompt, dtype=torch.float))
                     all_label_prompt.append(torch.as_tensor(label_prompt, dtype=torch.int))
 
             if ((point_prompt is None) and is_point_prompt) or ((mask_prompt is None) and is_mask_prompt):
@@ -420,7 +429,8 @@ class VideoDataset(data.Dataset):
             
         # if all_mask_prompt is not None:
         #     print(all_mask_prompt, all_mask_prompt[0], all_mask_prompt[0].shape, sep="\n")
-            
+       
+       
         return {
             "image": imgs, # Tensor(3, H (transformed), 1024)
             "gt_mask":  torch.stack(all_gt_copy), # Tensor(n, H, W)
@@ -449,33 +459,35 @@ def collate_fn(batch):
 
 # dataloader for training
 def get_loader(
-    dataset="../root",
+    cfg,
+    root_dir="raw",
     batchsize=1,
     seq_len=4,
-    iou_range = [0.05, 0.4],
+    iou_range = [0.15, 0.5],
     max_prompt_points = 3,
     trainsize=1024,
     num_workers=0,
     pin_memory=True,
     collate_fn=collate_fn,
+    
 ):
-    train_dataset = VideoDataset(dataset, seq_len, trainsize, iou_range=iou_range, max_prompt_points=max_prompt_points, finetune=True, is_validation_prompt_mid = False)
+    train_dataset = VideoDataset(root_dir, seq_len, trainsize, iou_range=iou_range, max_prompt_points=max_prompt_points, finetune=True, is_validation_prompt_mid = False)
     train_data_loader = data.DataLoader(
         dataset=train_dataset,
-        batch_size=batchsize,
+        batch_size=cfg.batch_size,
         shuffle=True,
-        num_workers=num_workers,
-        pin_memory=pin_memory,
+        num_workers=cfg.num_workers,
+        pin_memory=cfg.pin_memory,
         collate_fn=collate_fn,
     )
     
-    val_dataset = VideoDataset(dataset, seq_len, trainsize, iou_range=iou_range, max_prompt_points=max_prompt_points, finetune=False, is_validation_prompt_mid = True)
+    val_dataset = VideoDataset(root_dir, seq_len, trainsize, iou_range=iou_range, max_prompt_points=max_prompt_points, finetune=False, is_validation_prompt_mid = True)
     val_data_loader = data.DataLoader(
         dataset=val_dataset,
-        batch_size=batchsize,
+        batch_size=cfg.batch_size,
         shuffle=False,
-        num_workers=num_workers,
-        pin_memory=pin_memory,
+        num_workers=cfg.num_workers,
+        pin_memory=cfg.pin_memory,
         collate_fn=collate_fn,
     )
     
