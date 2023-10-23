ed16b8c20c75d988225b5367b91646130058c265
Pratyush <96726614+pratyush-1@users.noreply.github.com>
2023-10-24 00:19:20 +0530

    Add files via upload

--- train.py
+++ train.py
@@ -1,164 +1,184 @@
 import lightning as L
+from lightning.pytorch import LightningDataModule
 import torch
 import wandb
 from lightning.pytorch.loggers import WandbLogger
-from lightning.pytorch.callbacks import ModelCheckpoint, Callback
+from lightning.pytorch.callbacks import Callback, ModelSummary, LearningRateMonitor
 from omegaconf import OmegaConf
-
+from lightning.pytorch.tuner import Tuner
+import torch.utils.data as data
 from segment_anything import sam_model_registry
 from segment_anything.modeling import CamoSam
-import sys
-sys.path.append("dataloaders/")
-from dataloaders.MoCA import get_loader
+from lightning.pytorch.utilities.rank_zero import rank_zero_only, rank_zero_warn
 
-# from dataloaders.vos_dataset import get_loader
+from dataloaders.camo_dataset import get_loader
 
 import os
+torch.set_float32_matmul_precision('medium')
 
-# supported measures: 'MAE'       => Mean Squared Error
-#                     'E-measure' =>  Enhanced-alignment measure
-#                     'S-measure' =>  Structure-measure
-#                     'Max-F'     =>  Maximum F-measure
-#                     'Adp-F'     =>  Adaptive F-measure
-#                     'Wgt-F'     =>  Weighted F-measure
-
-############## MoCA
+# DAVIS
 config = {
-    "model_checkpoint_at": "checkpoint/",
-    "save_log_weights_interval":2,
-    "precision": 32,
-    "seq_len": 4,
+    "description": "Adapter Training",
+    "precision": "32",
     "num_devices": 1,
-    "batch_size": 2,
-    "num_workers": 0,
-    "num_epochs": 20,
-    "max_prompt_points":2,
-    "iou_range": [0.02, 0.2], # mask prompt
-    "metric_train_eval_interval": 20,
-    "log_every_n_steps": 1,
-    "log_metrics": ['MAE', 'E-measure', 'S-measure', 'Max-F', 'Adp-F', 'Wgt-F'],
+    "num_epochs": 300,
+    "save_log_weights_interval": 25,
+    "train_metric_interval": 25,
+    "model_checkpoint_at": "checkpoints",
     "img_size": 1024,
     "out_dir": "/",
+    "focal_wt": 20,
+    "num_tokens": 1,
     "opt": {
-        "learning_rate": 8e-4,
+        "learning_rate": 4e-4, #1e-4
+        "auto_lr": False,
         "weight_decay": 1e-4,
-        "decay_factor": 10,
-        "steps": [60000, 86666],
-        "warmup_steps": 250,
+        "decay_factor": 1/2,
+        "steps": [100, 250],
     },
     "model": {
-        "type": "vit_b",
-        "checkpoint": "sam_vit_b_01ec64.pth",
-        "freeze": {
-            "image_encoder": True,
-            "prompt_encoder": True,
-            "mask_decoder": True,
+        "type": "vit_l",
+        "checkpoint": "sam_vit_l_0b3195.pth",
+        "requires_grad": {
+            "image_encoder": False,
+            "prompt_encoder": False,
+            "mask_decoder": False,
+            "propagation_module": False,
         },
     },
     "dataset": {
-        "train": {
-            "root_dir": "raw/MoCA-Mask/MoCA_Video",
-        },
-        "val": {
-            "root_dir": "raw/MoCA-Mask/MoCA_Video",
-        },
+        "root_dir": "raw/",
+        "batch_size": 1,
+        "num_workers": 4,
+        "pin_memory": False,
+        "persistent_workers": True,
     },
 }
-############## DAVIS
-# config = {
-#     "train_split": 0.93,
-#     "precision": 32,
-#     "seq_len": 4,
-#     "num_devices": 1,
-#     "batch_size": 2,
-#     "num_workers": 0,
-#     "num_epochs": 50,
-#     "augmentation_fps_max_frame_skip":5,
-#     "metric_train_eval_interval": 20,
-#     "log_every_n_steps": 1,
-#     "log_metrics": ['MAE', 'E-measure', 'S-measure', 'Max-F', 'Adp-F', 'Wgt-F'],
-#     "img_size": 1024,
-#     "out_dir": "/",
-#     "opt": {
-#         "learning_rate": 8e-4,
-#         "weight_decay": 1e-4,
-#         "decay_factor": 10,
-#         "steps": [60000, 86666],
-#         "warmup_steps": 250,
-#     },
-#     "model": {
-#         "type": "vit_b",
-#         "checkpoint": "sam_vit_b_01ec64.pth",
-#         "freeze": {
-#             "image_encoder": True,
-#             "prompt_encoder": True,
-#             "mask_decoder": True,
-#         },
-#     },
-#     "dataset": {
-#             "root_dir": "raw/DAVIS/",
+cfg = OmegaConf.create(config)
 
-#     },
-# }
+# api = wandb.Api()
+# artifact = api.artifact('spider-r-d/DAVIS Propagation/model_cyzxvd5f:v26', type='model')
+# artifact_dir = artifact.download()
 
+# ckpt = torch.load('artifacts/model_cyzxvd5f:v26/499_epoch_7500_global_step.pth')
+ckpt = None
+
+# First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = sam_model_registry[cfg.model.type](checkpoint=cfg.model.checkpoint, cfg=cfg)
+model = CamoSam(cfg, model, ckpt=ckpt)
+# model = torch.compile(model, mode="reduce-overhead")
+
+# class LitDataModule(LightningDataModule):
+#     def __init__(self, batch_size):
+#         super().__init__()
+#         self.save_hyperparameters()
+#         # or
+#         self.batch_size = batch_size
+#         self.cfg = cfg
+
+#     def train_dataloader(self):
+#         with open(self.cfg.dataset.root_dir+'ImageSets/2017/train.txt', 'r') as file:
+#             train_list = [line.strip() for line in file]
+#         print("Training Samples: ",len(train_list))
+
+#         train_dataset = VOSDataset(self.cfg.dataset.root_dir+'JPEGImages/Full-Resolution', 
+#                                    self.cfg.dataset.root_dir+'Annotations/Full-Resolution', 
+#                                    train_list ,max_jump=self.cfg.dataset.max_jump, 
+#                                    num_frames=self.cfg.dataset.num_frames,  
+#                                    max_num_obj=self.cfg.dataset.max_num_obj, 
+#                                    val=False)
+#         train_data_loader = data.DataLoader(
+#             dataset=train_dataset,
+#             batch_size=self.batch_size,
+#             shuffle=True,
+#             # num_workers=self.cfg.dataset.num_workers,
+#             # pin_memory=self.cfg.dataset.pin_memory,
+#         )
+        
+#         return train_data_loader
+    
+#     def val_dataloader(self):
+
+#         with open(self.cfg.dataset.root_dir+'ImageSets/2017/val.txt', 'r') as file:
+#             val_list = [line.strip() for line in file]
+#         print("Validation Samples: ",len(val_list))
+
+#         val_dataset = VOSDataset(self.cfg.dataset.root_dir+'JPEGImages/Full-Resolution', 
+#                                  self.cfg.dataset.root_dir+'Annotations/Full-Resolution', 
+#                                  val_list, max_jump=self.cfg.dataset.max_jump, 
+#                                  num_frames=self.cfg.dataset.num_frames,  
+#                                  max_num_obj=self.cfg.dataset.max_num_obj, 
+#                                  val=True)
+#         val_data_loader = data.DataLoader(
+#             dataset=val_dataset,
+#             batch_size=self.batch_size,
+#             shuffle=False,
+#             # num_workers=self.cfg.dataset.num_workers,
+#             # pin_memory=self.cfg.dataset.pin_memory,
+#         )
+
+#         return val_data_loader
 
 class WandB_Logger(Callback):
     def __init__(self, cfg, wb):
         self.logged_weight_epoch = 0
         self.cfg = cfg
         self.wb = wb
-
+        
+        if cfg.model_checkpoint_at not in os.listdir():
+            os.mkdir(cfg.model_checkpoint_at)
+            
+    @rank_zero_only
     def on_validation_epoch_end(self, trainer, pl_module):
-        if (pl_module.current_epoch) % self.cfg.save_log_weights_interval == 0 and pl_module.current_epoch >0:
-            model_name = f"{os.path.join(cfg.model_checkpoint_at, f'{pl_module.current_epoch}_epoch_{trainer.global_step}_global_step.pth')}"
-            torch.save({
-                        'cfg': self.cfg,
-                        'epoch': pl_module.current_epoch,
-                        'model_state_dict': pl_module.model.state_dict(),
-                        'optimizer_state_dict': pl_module.optimizers().state_dict() if type(pl_module.optimizers())!=list else {},
-                        'benchmark': [pl_module.train_benchmark, pl_module.val_benchmark],
-            }, model_name)
-            print("UPLOADING MODEL TO W&B", model_name)
-            my_model = wandb.Artifact(f"model_{self.wb.id}", type="model")
-            my_model.add_file(model_name)
-            self.wb.log_artifact(my_model)
-            # Link the model to the Model Registry
-            self.wb.link_artifact(my_model, f"Adapter/TEST")
+        model_name = f"{os.path.join(cfg.model_checkpoint_at, f'{pl_module.current_epoch}_epoch_{trainer.global_step}_global_step.pth')}"
+        
+        if pl_module.current_epoch == 0:
+            pl_module.train_benchmark = []
+            pl_module.val_benchmark = []
+        else:
+            pl_module.train_benchmark = sum(pl_module.train_benchmark) / len(pl_module.train_benchmark)
+            pl_module.val_benchmark = sum(pl_module.val_benchmark) / len(pl_module.val_benchmark)
+        
+        torch.save({
+                    'cfg': self.cfg,
+                    # 'epoch': pl_module.current_epoch,
+                    # 'adapter_state_dict': pl_module.model.image_encoder.adapter.state_dict(),
+                    # 'feature_extractor_state_dict':pl_module.model.image_encoder.feature_extractors.state_dict(),
+                    'optimizer_state_dict': pl_module.optimizers().state_dict() if type(pl_module.optimizers())!=list else {},
+                    'benchmark': [pl_module.train_benchmark, pl_module.val_benchmark],
+        }, model_name)
+        my_model = wandb.Artifact(f"model_{self.wb.id}", type="model")
+        my_model.add_file(model_name)
+        self.wb.log_artifact(my_model)
+        # Link the model to the Model Registry
+        # self.wb.link_artifact(my_model, f"DAVIS Propagation/Model_arch_1")
+            
+        pl_module.train_benchmark = []
+        pl_module.val_benchmark = []
 
-# First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
-cfg = OmegaConf.create(config)
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model = sam_model_registry['vit_b'](checkpoint="sam_vit_b_01ec64.pth", cfg=cfg)
-model = CamoSam(cfg, model)
-# resume_path = "/content/drive/MyDrive/Group3/sam-finetuning/sam_vit_b_01ec64.pth"
-# model.load_state_dict(load_state_dict(resume_path, location='cpu'), strict=False)
-
-
-# MoCA
-train_dataloader, validation_dataloader = get_loader(cfg.dataset.train.root_dir, cfg.batch_size, cfg.seq_len, cfg.iou_range, cfg.max_prompt_points)
-
-# DAVIS
-# train_dataloader, validation_dataloader = get_loader(cfg.dataset.root_dir, 
-#                                                      train_split=cfg.train_split,
-#                                                     batchsize= cfg.batch_size,  
-#                                                     max_jump = cfg.augmentation_fps_max_frame_skip,
-#                                                     num_frames=cfg.seq_len,  
-#                                                     max_num_obj=1, )
-
-wandblogger = WandbLogger(project="Adapter")
-wandblogger.experiment.config.update(config)
-
+# torch._dynamo.config.verbose=True # for debugging
+wandblogger = WandbLogger(project="adapter_training", save_code=True, settings=wandb.Settings(code_dir="."))
 model_weight_callback = WandB_Logger(cfg, wandblogger.experiment)
+lr_monitor = LearningRateMonitor(logging_interval='step')
 
+# datamodule = LitDataModule(cfg.dataset.batch_size)
 trainer = L.Trainer(
     accelerator=device,
-    callbacks=[model_weight_callback],
+    devices=cfg.num_devices,
+    callbacks=[model_weight_callback, ModelSummary(max_depth=2), lr_monitor],
     precision=cfg.precision,
     logger=wandblogger,
     max_epochs=cfg.num_epochs,
     # strategy="ddp",
-    log_every_n_steps=cfg.log_every_n_steps,
+    log_every_n_steps=15,
+    check_val_every_n_epoch=cfg.train_metric_interval,
+    enable_checkpointing=False,
+    profiler='simple',
+    # overfit_batches=1
 )
+if trainer.global_rank == 0:
+    wandblogger.experiment.config.update(config)
 
-trainer.validate(model, validation_dataloader)
+train_dataloader, validation_dataloader = get_loader(cfg.dataset)
 trainer.fit(model, train_dataloader, validation_dataloader)
